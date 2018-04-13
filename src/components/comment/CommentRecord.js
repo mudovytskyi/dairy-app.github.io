0c1877d5bd963782260ed4c538e56df933d4a2c8
mudovytskyi <mudovytskyi@gmail.com>
2018-04-13 23:53:58 +0300

    clean up css , update lists, add scrolls to lists

--- src/components/comment/CommentRecord.js
+++ src/components/comment/CommentRecord.js
@@ -9,12 +9,12 @@ import CommentIcon from './CommentIcon';
 class CommentRecord extends Component {
     render () {
         return (
-            <div className="CommentRecord">
+            <li className="CommentRecord">
                 <CommentIcon />
                 <div className="comment-name">
                     <span>{this.props.value}</span>
                 </div>
-            </div>
+            </li>
         );
     }
 }
